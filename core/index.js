9755fdee56135fbf2ace01419c552e91d3f682d1
Felisong <carolinahs100@gmail.com>
2026-04-09 20:23:32 -0600

    refined my start up for the server so that it never crashes and should always be ready on boot up

--- core/index.js
+++ core/index.js
@@ -1,155 +1,82 @@
-require('dotenv').config()
-
-const express = require('express');
-const axios = require('axios');
-const fs = require('fs');
-const path = require('path');
-const WebSocket = require('ws');
-const {router, broadcastToCatSpawner} = require('./routes/projects');
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+const { startUp } = require("./startupHelper");
+const { router } = require("./routes/projects");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const SCOPES = [
-  'channel:read:redemptions',
-  'channel:manage:redemptions',
-  'chat:read',
-  'chat:edit',
-  'channel:read:subscriptions',
-  'channel:read:hype_train',
-  'moderator:read:followers',
-  'bits:read',
-  'channel:manage:raids'
-].join(' ')
+  "channel:read:redemptions",
+  "channel:manage:redemptions",
+  "chat:read",
+  "chat:edit",
+  "channel:read:subscriptions",
+  "channel:read:hype_train",
+  "moderator:read:followers",
+  "bits:read",
+  "channel:manage:raids",
+].join(" ");
 
-app.use('/projects', router);
-app.use('/cat-spawner', express.static(path.join(__dirname, '../assets/cat-spawner')))
+app.use("/projects", router);
+app.use(
+  "/cat-spawner",
+  express.static(path.join(__dirname, "../assets/cat-spawner")),
+);
 
-app.get('/', (req, res) => {
-  res.send.JSON({status: 'healthy', informationNeeded: {unknown: "no notes!"}});
-})
+app.get("/", (req, res) => {
+  res.send.JSON({
+    status: "healthy",
+    informationNeeded: { unknown: "no notes!" },
+  });
+});
 // redirect to twitch login
-app.get('/auth', (req, res) => {
-   console.log(`test i should make it here`, process.env.TWITCH_CLIENT_ID);
-    const url =  `https://id.twitch.tv/oauth2/authorize?client_id=${process.env.TWITCH_CLIENT_ID}&redirect_uri=${process.env.TWITCH_REDIRECT_URI}&response_type=code&scope=${SCOPES}`;
-    res.redirect(url);
-})
+app.get("/auth", (req, res) => {
+  const url = `https://id.twitch.tv/oauth2/authorize?client_id=${process.env.TWITCH_CLIENT_ID}&redirect_uri=${process.env.TWITCH_REDIRECT_URI}&response_type=code&scope=${SCOPES}`;
+  res.redirect(url);
+});
 
 // twitch sends us back here with a code
-app.get('/auth/callback', async (req, res) => {
-    const code = req.query.code;
-   
-
-    try {
-        const response = await axios.post('https://id.twitch.tv/oauth2/token', null, {
-            params: {
-                client_id: process.env.TWITCH_CLIENT_ID,
-                client_secret: process.env.TWITCH_CLIENT_SECRET,
-                code,
-                grant_type: 'authorization_code',
-                redirect_uri: process.env.TWITCH_REDIRECT_URI
-            }
-        })
-
-        const tokens = response.data;
-        console.log(`tokens: `, JSON.stringify(tokens, null, 2));
-        fs.writeFileSync(path.join(__dirname, 'tokens.json'), JSON.stringify(tokens, null, 2));        
-        console.log('Tokens saved!')
-        res.send('Auth complete! You can close this tab.');
-
-    } catch (err){
-        console.error(err);
-        res.send('Something went wrong during retrieving data');
-    }
-})
-
-
-
-// REFRESH TOKEN LOGIC
-async function refreshAccessToken(){
-    const tokens = json.parse(fs.readFileSync(path.join(__dirname + 'tokens.json')));
+app.get("/auth/callback", async (req, res) => {
+  const code = req.query.code;
 
   try {
-    const response = await axios.post('https://id.twitch.tv/oauth2/token', null, {
-      params: {
-        client_id: process.env.TWITCH_CLIENT_ID,
-        client_secret: process.env.TWITCH_CLIENT_SECRET,
-        grant_type: 'refresh_token',
-        refresh_token: tokens.refresh_token
-      }
-    })
-
-    const newTokens = response.data
-    fs.writeFileSync(path.join(__dirname, 'tokens.json'), JSON.stringify(newTokens, null, 2))
-    console.log('Tokens refreshed!')
-    return newTokens.access_token
-
+    const response = await axios.post(
+      "https://id.twitch.tv/oauth2/token",
+      null,
+      {
+        params: {
+          client_id: process.env.TWITCH_CLIENT_ID,
+          client_secret: process.env.TWITCH_CLIENT_SECRET,
+          code,
+          grant_type: "authorization_code",
+          redirect_uri: process.env.TWITCH_REDIRECT_URI,
+        },
+      },
+    );
+    const tokens = response.data;
+    tokens.expires_at = Date.now() + tokens.expires_in * 1000;
+    fs.writeFileSync(
+      path.join(__dirname, "tokens.json"),
+      JSON.stringify(tokens, null, 2),
+    );
+    console.log("Tokens saved!");
+    res.send("Auth complete! You can close this tab.");
   } catch (err) {
-    console.error('Failed to refresh token:', err)
-}}
+    console.error(err);
+    res.send("Something went wrong during retrieving data");
+  }
+});
 
-// WEB SOCKET FOR EVENT SUB
-async function connectEventSub() {
-  const tokens = JSON.parse(fs.readFileSync(path.join(__dirname, 'tokens.json')))
-  const ws = new WebSocket('wss://eventsub.wss.twitch.tv/ws')
 
-  ws.on('open', () => {
-    console.log('Connected to Twitch EventSub!')
-  })
 
-  ws.on('message', async (data) => {
-    const msg = JSON.parse(data)
-    const type = msg.metadata?.message_type
-
-    if (type === 'session_welcome') {
-      const sessionId = msg.payload.session.id
-      console.log('Session ID:', sessionId)
-      await subscribeToEvents(sessionId, tokens.access_token)
-    }
-
-    if (type === 'notification') {
-      const event = msg.payload.event
-      const rewardTitle = msg.payload.subscription.type
-      console.log('Event received:', rewardTitle, event)
-
-      broadcastToCatSpawner({reward: event.reward, user: event.user_name, input: event.user_input});
-    }
-  })
-
-  ws.on('close', () => {
-    console.log('EventSub disconnected, reconnecting in 5s...')
-    setTimeout(connectEventSub, 5000)
-  })
-}
-
-async function subscribeToEvents(sessionId, accessToken) {
-  const broadcasterId = process.env.TWITCH_BROADCASTER_ID
-
-  await axios.post('https://api.twitch.tv/helix/eventsub/subscriptions', {
-    type: 'channel.channel_points_custom_reward_redemption.add',
-    version: '1',
-    condition: { broadcaster_user_id: broadcasterId },
-    transport: {
-      method: 'websocket',
-      session_id: sessionId
-    }
-  }, {
-    headers: {
-      'Authorization': `Bearer ${accessToken}`,
-      'Client-Id': process.env.TWITCH_CLIENT_ID,
-      'Content-Type': 'application/json'
-    }
-  })
-
-  console.log('Subscribed to channel point redemptions!')
-}
-
-connectEventSub()
-
+// On start handler depending on the existence of the token.json
+startUp();
 
 // listener
 app.listen(PORT, () => {
   console.log(`Stream hub is running on http://localhost:${PORT}`);
   console.log(`Authorize at http://localhost:${PORT}/auth`);
-})
-
-setInterval(refreshAccessToken, 1000 * 60 * 60);
+});
---
+++ core/startupHelper.js
@@ -0,0 +1,129 @@
+// hey me! I am making this to assure that no matter what state the tokens.json file is in, this should always boot upa and be working
+/* 
+If it doesnt even exist -> visit auth url -> authorize -> write the file -> set the refresh token timeout -> subscribe to events.
+if it does exist and its EXPIRED -> refresh token timeout -> subscribe to events
+if it does exis tand works -> subscribe to events and sets token timeout.
+ */
+const fs = require("fs");
+const WebSocket = require("ws");
+const path = require("path");
+const axios = require("axios");
+const {broadcastToCatSpawner} = require("./routes/projects");
+
+async function refreshAccessToken() {
+  const tokens = json.parse(
+    fs.readFileSync(path.join(__dirname, "tokens.json")),
+  );
+
+  try {
+    const response = await axios.post(
+      "https://id.twitch.tv/oauth2/token",
+      null,
+      {
+        params: {
+          client_id: process.env.TWITCH_CLIENT_ID,
+          client_secret: process.env.TWITCH_CLIENT_SECRET,
+          grant_type: "refresh_token",
+          refresh_token: tokens.refresh_token,
+        },
+      },
+    );
+
+    const newTokens = response.data;
+    fs.writeFileSync(
+      path.join(__dirname, "tokens.json"),
+      JSON.stringify(newTokens, null, 2),
+    );
+    console.log("Tokens refreshed!");
+    return newTokens.access_token;
+  } catch (err) {
+    console.error("Failed to refresh token:", err);
+  }
+}
+
+// connect to subscription
+// I will use this
+function connectToMyEventSub() {
+  // WEB SOCKET FOR EVENT SUB
+  async function getEvents() {
+    const tokens = JSON.parse(
+      fs.readFileSync(path.join(__dirname, "tokens.json")),
+    );
+    const ws = new WebSocket("wss://eventsub.wss.twitch.tv/ws");
+
+    ws.on("open", () => {
+      console.log("Connected to Twitch EventSub!");
+    });
+
+    ws.on("message", async (data) => {
+      const msg = JSON.parse(data);
+      const type = msg.metadata?.message_type;
+
+      if (type === "session_welcome") {
+        const sessionId = msg.payload.session.id;
+        console.log("Session ID:", sessionId);
+        await subscribeToEvents(sessionId, tokens.access_token);
+      }
+
+      if (type === "notification") {
+        const event = msg.payload.event;
+        const rewardTitle = msg.payload.subscription.type;
+        console.log("Event received:", rewardTitle, event);
+
+        broadcastToCatSpawner({
+          reward: event.reward,
+          user: event.user_name,
+          input: event.user_input,
+        });
+      }
+    });
+
+    ws.on("close", () => {
+      console.log("EventSub disconnected, reconnecting in 5s...");
+      setTimeout(getEvents, 5000);
+    });
+  }
+
+  async function subscribeToEvents(sessionId, accessToken) {
+    const broadcasterId = process.env.TWITCH_BROADCASTER_ID;
+
+    await axios.post(
+      "https://api.twitch.tv/helix/eventsub/subscriptions",
+      {
+        type: "channel.channel_points_custom_reward_redemption.add",
+        version: "1",
+        condition: { broadcaster_user_id: broadcasterId },
+        transport: {
+          method: "websocket",
+          session_id: sessionId,
+        },
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Client-Id": process.env.TWITCH_CLIENT_ID,
+          "Content-Type": "application/json",
+        },
+      },
+    );
+
+    console.log("Subscribed to channel point redemptions!");
+  }
+  // execute
+  getEvents();
+}
+
+async function startUp() {
+  if (fs.existsSync(path.join(__dirname, "tokens.json"))) {
+    const tokens = JSON.parse(
+      fs.readFileSync(path.join(__dirname, "tokens.json")),
+    );
+    if (tokens.expires_at && Date.now() > tokens.expires_at) await refreshAccessToken();
+    connectToMyEventSub();
+    setInterval(refreshAccessToken, 1000 * 60 * 60);
+  } else {
+    console.log(`Please visit http://localhost:3000/auth`)
+  }
+}
+
+module.exports = { refreshAccessToken, connectToMyEventSub, startUp };
