03efd17a49046014b4b77ece36d778c2ed7d4f78
Felisong <carolinahs100@gmail.com>
2026-06-14 23:08:47 -0600

    got the cat to spawn properly and figured out fundementally the minmax values for its movements.

--- cat-spawner/js/index.js
+++ cat-spawner/js/index.js
@@ -15,16 +15,28 @@ eventSource.onmessage = (e) => {
   // add a blocker so that it only does this is the event is what i want it to be
   if (currentEvent.connected) return;
   //   if (currentEvent.event.id !== '') return;
-  // I had a cat object I can set up right here.
-  console.log(`event:`, currentEvent);
-  activeCats.set(`${currentEvent.user}`, {
-    name: currentEvent.user,
-    color: currentEvent.input || "white",
-    xPos: 90,
-    yPos: 90,
-    scale: 1,
-    state: "spawn",
-  });
+  // y axis starts 5-100
+  // x axis is 0-95
+  if (!activeCats.get(`${currentEvent.user}`)) {
+    // if this is a new cat.  Creates the cat!
+    activeCats.set(`${currentEvent.user}`, {
+      name: currentEvent.user,
+      color: currentEvent.input || "white",
+      xPos: 95,
+      yPos: 100,
+      scale: 1,
+      state: "spawn",
+      opacity: 100,
+    });
+  } else {
+    // this is how I can update position later too.
+    activeCats.set(`${currentEvent.user}`, {
+      ...activeCats.get(`${currentEvent.user}`), // keep existing fields
+      color: currentEvent.input || "white", // overwrite only these
+      opacity: 100,
+    });
+  }
+
   console.log(`got the message and updated cats`);
 
   // create new cat
@@ -41,9 +53,7 @@ function spawnCat(currentCat) {
   const catContainer = document.createElement("div");
   const catName = document.createElement("p");
   const cat = document.createElement("div");
-  const catContainerDimensions = catContainer.getBoundingClientRect();
 
-  
   catName.innerText = currentCat.name;
 
   cat.style.backgroundColor = currentCat.color;
@@ -55,13 +65,27 @@ function spawnCat(currentCat) {
   catContainer.style.position = "absolute";
   catContainer.style.left = `0px`;
   catContainer.style.top = `0px`;
-  catContainer.style.transform = `translate(${Math.round((currentCat.xPos / 100) * windowWidth)}px, ${Math.round((currentCat.yPos / 100) * windowHeight)}px)`;
+  catContainer.style.opacity = `0%`;
 
   catContainer.appendChild(catName);
   catContainer.appendChild(cat);
 
   playArea.appendChild(catContainer);
+  const catContainerDimensions = catContainer.getBoundingClientRect();
+  const translateX = Math.round((currentCat.xPos / 100) * windowWidth);
+  const translateY = Math.round(
+    (currentCat.yPos / 100) * windowHeight - catContainerDimensions.height,
+  );
+
+  // child now exists. position
+  catContainer.style.transform = `translate(${translateX}px, ${translateY}px)`;
+  catContainer.style.opacity = `${currentCat.opacity}%`;
 }
+
+
+
+
+
 
 // environment loop
 function environmentLoop() {
